289209252c2617fd27dc8b7e2fd9ea77fb7c1da4
debaser19 <steve@stoveld.xyz>
2020-08-09 21:01:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import json
 import pandas as pd
 import creds
+from datetime import date
 
 def getTodaysGames():
     print('Getting list of games for today...')
@@ -148,5 +149,116 @@ def doMagic():
     return r
 
 
-doMagic()
+def createScoreboard():
+    games = getTodaysGames()
+
+    style_content = '''
+    <style>
+    table{border-collapse:collapse;}
+    table,th,td{border:1px solid;padding:5px;}
+    td:first-child{width:170px;}
+    td:last-child{width:70px;text-align:center;}
+    </style>
+    '''
+    table_content = ''
+    for game in games:
+        # format the period
+        if (int(game['period'])-2) not in range(1,3):
+            game['period'] = ''
+        elif (int(game['period'])-2) == 4:
+            game['period'] = 'OT'
+        else:
+            game['period'] = f'Period {int(game["period"])-2}'
+        table_content += f'''
+        <p>
+        <table>
+        <thead>
+        <tr>
+        <th>Teams</th>
+        <th>Score</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+        <td>{game['away_team']}</td>
+        <td>{game['away_score']}</td>
+        </tr>
+        <tr>
+        <td>{game['home_team']}</td>
+        <td>{game['home_score']}</td>
+        </tr>
+        <tr>
+        <td>{game['game_status']}</td>
+        <td>{game['period']}</td>
+        </tr>
+        </tbody>
+        </table>
+        </p>
+        '''
+    post_content = style_content + table_content
+
+    return post_content
+
+
+def getCurrentDate():
+    current_date = date.today()
+    month = current_date.month
+    day = current_date.day
+    year = current_date.year
+    formatted_date = f'{month}/{day}/{year}'
+
+    return formatted_date
+
+
+def postScoreboard():
+    apikey = creds.creds['apikey']
+    url = f'http://leafsconnected.com/api/forums/topics?key={apikey}'
+
+    payload = {
+        "forums": [56]
+    }
+
+    r = requests.get(url,params=payload)
+    data = r.json()
+    topics = data['results']
+    
+    list_of_topics = []
+    for topic in topics:
+        topic_dict = {
+            "id": topic['id'],
+            "title": topic['title']
+        }
+        list_of_topics.append(topic_dict)
+
+    topic_title = f'[{getCurrentDate()}] Out of Town Scoreboard'
+
+    post_content = f'''
+        <h1>{topic_title}</h1>
+        {createScoreboard()}
+        '''
+    
+    # check if today's post is already created
+    if topic['title'] != topic_title:
+        print(topic['title'])
+        print("Creating topic...")
+        payload = {
+            "forum": 56,
+            "title": topic_title,
+            "post": post_content,
+            "author": 2,
+            "pinned": 1
+        }
+        r = requests.post(url,data=payload)
+    else:
+        # create it if it doesn't exist
+        print("Updating post...")
+        post_id = topic['firstPost']['id']
+        url = f'http://leafsconnected.com/api/forums/posts/{post_id}?key={apikey}'
+        payload = {
+            "post": post_content
+        }
+        r = requests.post(url,data=payload)
+
+
+postScoreboard()
 
